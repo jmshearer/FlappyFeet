041f3a136e4b404b5c42a88c245308748bcbbf72
Michael Shearer <michael@gtpcorp.local>
2023-10-20 13:43:18 -0400

    Improve graphics, clean up file names

--- Game/js/flappyfeet.js
+++ Game/js/flappyfeet.js
@@ -11,22 +11,28 @@ window.addEventListener("gamepaddisconnected", function(e) {
     console.log("Gamepad disconnected!", e.gamepad);
 });
 
+gamepadPresent=false;
+inhibitUpdates=false;
+
 function updateJoystick() {
     var gamepadList = navigator.getGamepads();
 
-    if (gamepadList[0]) {
+    if (gamepadList[0]) {			
         // Assuming the first gamepad is the one you're interested in.
         // Axis 1 is typically the y-position of the left thumbstick
         var yPosition = gamepadList[0].axes[1];
         curJoyY = yPosition;
         const canvas = document.getElementById('flappyCanvas');
+				gamepadPresent=true;
         //bird.y = canvas.height * yPosition + .5;
         //document.getElementById("out").innerHTML = yPosition.toFixed(0);
-        console.log("Joystick Y Position:", yPosition.toFixed(0));
+        //console.log("Joystick Y Position:", yPosition.toFixed(0));
 
         // If yPosition is 0, the joystick is centered. 
         // Negative values indicate up, positive values indicate down.
-    }
+    } else {
+			gamepadPresent=false;
+		}
 
     // Keep updating as long as the gamepad is connected
     requestAnimationFrame(updateJoystick);
@@ -40,7 +46,7 @@ function updateJoystick() {
     let bird = {
         x: canvas.width / 5,
         y: canvas.height / 2,
-        size: 20,
+        size: 40,
         dx: 0,
         dy: 0,
         speed: 4
@@ -48,26 +54,48 @@ function updateJoystick() {
 
     let pipes = [];
     let score = 0;
-    const pipeWidth = 50;
+    const pipeWidth = 75;
     const gapHeight = 300;
-    let pipeGap = 280;  // You can adjust this value for the desired gap between pipes.
+    let pipeGap = 320;  // You can adjust this value for the desired gap between pipes.
 		
 		const sprite = new Image();
 		sprite.src = "assets/sprites.png";
 
+		spriteSeq=0;
+
     function drawBird() {
-        //ctx.fillStyle = 'yellow';
-        //ctx.beginPath();
-        //ctx.arc(bird.x, bird.y, bird.size, 0, Math.PI * 2);
-        //ctx.closePath();
-        //ctx.fill();
-				ctx.drawImage(sprite, 2, 490, 19, 14, bird.x,bird.y,60, 60);
+		
+	        /*ctx.fillStyle = 'yellow';
+      ctx.beginPath();
+      ctx.arc(bird.x, bird.y, bird.size, 0, Math.PI * 2);
+      ctx.closePath();
+      ctx.fill();*/
+			
+			drawSize = bird.size * 1.25;
+				
+			spriteSeq+=20;
+			//spriteSeq=310;
+			if(spriteSeq<100){
+				ctx.drawImage(sprite, 218, 299, 78, 78, bird.x-drawSize,bird.y-drawSize,drawSize*2, drawSize*2);
+			}else if(spriteSeq<200){
+				ctx.drawImage(sprite, 220, 435, 78, 78, bird.x-drawSize,bird.y-drawSize,drawSize*2, drawSize*2);
+			}else if(spriteSeq<300){
+				ctx.drawImage(sprite, 220, 369, 78, 78, bird.x-drawSize,bird.y-drawSize,drawSize*2, drawSize*2);
+			}else if(spriteSeq<400){
+				ctx.drawImage(sprite, 220, 435, 78, 78, bird.x-drawSize,bird.y-drawSize,drawSize*2, drawSize*2);	
+			}else{
+				ctx.drawImage(sprite, 218, 299, 78, 78, bird.x-drawSize,bird.y-drawSize,drawSize*2, drawSize*2);
+				spriteSeq=0;				
+			}
+			//ctx.drawImage(sprite, 220, 369, 78, 78, bird.x-bird.size,bird.y-bird.size,bird.size*2, bird.size*2);
+				
+
+				//ctx.drawImage(sprite, 2, 490, 19, 14, bird.x-bird.size,bird.y-bird.size,bird.size*2, bird.size*2);
     }
 
 		var parallax = 0;
     function drawPipes() {
-			var sScale = 256 / canvas.height;
-			console.log(parallax);
+			var sScale = 256 / canvas.height;			
 			parallax-=1;
 			if(parallax<=-Math.round(143/sScale)){
 				parallax=0;
@@ -82,9 +110,16 @@ function updateJoystick() {
             //ctx.fillStyle = 'green';
 						//ctx.fillRect(pipe.x, 0, pipeWidth, pipe.top);
 						//ctx.fillRect(pipe.x, pipe.top + gapHeight, pipeWidth, canvas.height - pipe.top - gapHeight);
+						var sScale = 27 / pipeWidth;
 												           						
-						ctx.drawImage(sprite, 55, 323, 28, 160, pipe.x, 0, pipeWidth, pipe.top);            						
-						ctx.drawImage(sprite, 84, 323, 27, 160, pipe.x, pipe.top + gapHeight, pipeWidth,  canvas.height - pipe.top - gapHeight);					
+						ctx.drawImage(sprite, 55, 323, 28, 140, pipe.x, 0, pipeWidth, pipe.top);
+						ctx.drawImage(sprite, 55, 463, 28, 20, pipe.x, pipe.top-20, pipeWidth, 20/sScale);											
+						//ctx.drawImage(sprite, 55, 323, 28, 160, pipe.x, 0, pipeWidth, pipe.top);
+						            						
+						//ctx.drawImage(sprite, 84, 323, 27, 160, pipe.x, pipe.top + gapHeight, pipeWidth,  canvas.height - pipe.top - gapHeight);
+						 						
+						ctx.drawImage(sprite, 84, 343, 27, 120, pipe.x, pipe.top + gapHeight, pipeWidth,  canvas.height - pipe.top - gapHeight);
+						ctx.drawImage(sprite, 84, 323, 27, 20, pipe.x, pipe.top + gapHeight, pipeWidth,  20 / sScale);					
 					
 
         });
@@ -116,20 +151,28 @@ function updateJoystick() {
                 bird.x + bird.size > pipe.x && bird.x - bird.size < pipe.x + pipeWidth &&
                 (bird.y - bird.size < pipe.top || bird.y + bird.size > pipe.top + gapHeight)
             ) {						
-                flashScreen();																
-                resetGame();
+                gameOver();
             }
         }
 
         if (bird.y + bird.size >= canvas.height || bird.y - bird.size <= 0 || bird.x - bird.size <= 0 || bird.x + bird.size >= canvas.width) {				
-            flashScreen();						
-            resetGame();
+            gameOver();
         }
     }
+		
+		function gameOver(){
+			flashScreen();
+			inhibitUpdates=true;
+			setTimeout(function(){
+				inhibitUpdates=false;			
+      	resetGame();
+			},2000);
+		}
 
     function flashScreen() {
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = "rgba(255,0,0,.5)";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
+				ctx.drawImage(sprite, 391, 58, 101, 26, canvas.width/2-200, canvas.height/2-50, 400, 100);
     }
 
     function resetGame() {
@@ -140,29 +183,40 @@ function updateJoystick() {
     }
 
     function update() {
+			if(!inhibitUpdates){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+                
+				if(gamepadPresent){
+        	bird.y = canvas.height * (curJoyY + 0.5);					
+				} else {
+					bird.x += bird.dx;
+	        bird.y += bird.dy;
+				}
         
-        bird.x += bird.dx;
-        bird.y += bird.dy;
-        bird.y = canvas.height * (curJoyY + 0.5);
-        
-        if(bird.y<50){
-            bird.y = 50;
+        if(bird.y<bird.size*1.1){
+            bird.y = bird.size*1.1;
         }
-        if(bird.y>canvas.height-50){
-            bird.y = canvas.height-50;
+        if(bird.y>canvas.height-bird.size*1.1){
+            bird.y = canvas.height-bird.size*1.1;
         }
+				
+				console.log(bird.y);
 
         drawPipes();
         movePipes();
         drawBird();
         checkCollisions();
 
-        ctx.fillStyle = 'black';
-        ctx.font = '24px Arial';
-        ctx.fillText('Score: ' + score, 10, 30);
-
-        requestAnimationFrame(update);
+        //ctx.fillStyle = 'black';
+        //ctx.font = '24px Arial';
+        //ctx.fillText('Score: ' + score, 10, 30);
+				
+				const scoreDiv = document.getElementById('score');
+				scoreDiv.innerHTML = score;
+					
+			}
+			
+    	requestAnimationFrame(update);
     }
 
     document.addEventListener('keydown', function(e) {
@@ -172,12 +226,12 @@ function updateJoystick() {
         if (e.key === 'ArrowDown' || e.keyCode === 40) {
             bird.dy = bird.speed;
         }
-        if (e.key === 'ArrowLeft' || e.keyCode === 37) {
+        /*if (e.key === 'ArrowLeft' || e.keyCode === 37) {
             bird.dx = -bird.speed;
         }
         if (e.key === 'ArrowRight' || e.keyCode === 39) {
             bird.dx = bird.speed;
-        }
+        }*/
     });
 
     document.addEventListener('keyup', function(e) {
